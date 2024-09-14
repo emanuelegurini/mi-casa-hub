e9177d4295da5dbe66f10e5fdcf016426cf0945c
EmanueleGurini <emanuele.gurini89@gmail.com>
2024-09-14 23:45:06 +0200

    feat(dashboard): add get properties abstract

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import { revalidatePath } from "next/cache";
+import { notFound } from "next/navigation";
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 
 export default async function Home() {
   const supabase = createClient();
@@ -9,12 +11,23 @@ export default async function Home() {
     data: { user },
   } = await supabase.auth.getUser();
 
-  console.log("user:", user);
-
   if (!user) {
     revalidatePath("/", "layout");
     redirect("/login");
   }
 
-  return <main>Hello, MICASA</main>;
+  // Fetch properties
+  let { data } = await supabase
+    .rpc("get_all_properties_abstract")
+    .throwOnError();
+
+  return (
+    <main>
+      <ul>
+        {data.map((p: any, i: any) => {
+          return <li key={"i"}>{p.title}</li>;
+        })}
+      </ul>
+    </main>
+  );
 }
